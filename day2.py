bfb3a4402302c8ae1acb317c4ce185ebc26a127b
Tanay Jaiswal <kira.x.tanay@gmail.com>
2026-05-04 19:55:12 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,12 @@
+print("Day 2: My python journey")
+name="Tanay Jaiswal"
+print(name)
+print("today I learned about string,lenght,memory and variable,numbers and some simple calculations")
+print("so I am going  use these all ")
+print(len("keepitup"))
+item="xiomi pad 8 pro"
+print(item)
+price = 25000
+discount = 0.2*price
+total = 25000-5000
+print(total) 
